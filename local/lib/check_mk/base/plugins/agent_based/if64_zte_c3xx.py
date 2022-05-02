a5878eb3a4cc2f755960c38465edbad0f2ad068c
WojRep <wrepinski@gmail.com>
2022-05-02 14:10:40 +0200

    update

--- local/lib/check_mk/base/plugins/agent_based/if64_zte_c3xx.py
+++ local/lib/check_mk/base/plugins/agent_based/if64_zte_c3xx.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 #
+from typing import List
+from cmk.base.plugins.agent_based.agent_based_api.v1 import (
+    all_of,
+    contains,
+    OIDBytes,
+    register,
+    SNMPTree,
+    type_defs,
+)
+from cmk.base.plugins.agent_based.utils import if64, interfaces
+
+def parse_if64_zte_c3xx(string_table: List[type_defs.StringByteTable]):
+    preprocessed_lines = []
+    for line in string_table[0]:
+        if not line[18]:
+            line[18] = line[1]
+        line = line[:20]
+        preprocessed_lines.append(line[:3] + [str(int(line[3]))] + line[4:])
+    return if64.generic_parse_if64([preprocessed_lines])
